639014bcf47b5f8c729a01b53005d0d943ad480a
bigyanpoudel <46457363+bigyanpoudel@users.noreply.github.com>
2022-07-26 15:06:43 +0545

    husky check

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { ROUTES } from './routes'
 function App() {
   console.log('hello world')
   console.log('hello world')
+  console.log('hello world')
   return (
     <BrowserRouter>
       <Routes>
