c0b6a960371ff4889f2c2498912da708578dc241
bigyanpoudel <bgyanpoudel11@gmail.com>
2022-08-01 11:56:23 +0545

    button class merge

--- src/component/atom/ModalActions/ModalActions.tsx
+++ src/component/atom/ModalActions/ModalActions.tsx
@@ -12,8 +12,8 @@ export const ModalActions = ({ className = '', actions }: IModalActionsProps) =>
   const classes = clsx(['modal-action border-t p-0 m-0 px-3 py-2', className])
   return (
     <div className={classes}>
-      {actions.map((el: IButtonProps, index: number) => (
-        <Button {...el} size="md" key={index} />
+      {actions?.map((el: IButtonProps, index: number) => (
+        <Button {...el} key={index} />
       ))}
     </div>
   )
