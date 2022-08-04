7743672689b4500cae95df99fab1527aede5757d
bigyanpoudel <46457363+bigyanpoudel@users.noreply.github.com>
2022-08-04 11:40:23 +0545

    modal and side pane

--- src/component/molecules/Modal/Modal.tsx
+++ src/component/molecules/Modal/Modal.tsx
@@ -28,30 +28,31 @@ export const Modal = forwardRef<HTMLDivElement, IModalProps>(
       onModalClose = () => {},
       hideCloseIcon = false,
       width,
+      isSlidePane = false,
+      position = 'right',
     }: IModalProps,
     ref,
   ): JSX.Element => {
-    const containerClasses = clsx([
-      'modal',
-      clsx({
-        'modal-open': open,
-        'modal-bottom sm:modal-middle': responsive,
-      }),
-    ])
-
-    const bodyClasses = clsx([
-      'modal-box rounded-md p-0 ',
+    const containerClasses = clsx('modal h-full', {
+      'modal-open': open,
+      'modal-bottom sm:modal-middle': responsive,
+    })
+    const w = '1200px'
+    const bodyClasses = clsx(
+      'modal-box rounded-md  p-0 flex flex-col max-h-full  overflow-hidden',
       className,
       {
         'max-w-[350px]': modalSize === 'xs',
         'max-w-[500px]': modalSize === 'sm',
         'max-w-[750px]': modalSize === 'md',
         'max-w-[90%]': modalSize === 'lg',
+        [`absolute top-0 max-h-full bottom-0`]: isSlidePane,
+        'right-0 rounded-r-none': isSlidePane && position === 'right',
+        'left-0': isSlidePane && position === 'left',
       },
-      `${width && `w-[${width}px]`}`,
-    ])
+    )
 
-    const contentClasses = clsx(['p-0 m-0 p-3', contentClassName])
+    const contentClasses = clsx('p-0 m-0 p-3 relative flex-1 overflow-y-auto', contentClassName)
 
     return (
       <div
@@ -67,7 +68,14 @@ export const Modal = forwardRef<HTMLDivElement, IModalProps>(
           }
         }}
       >
-        <div className={bodyClasses} ref={ref}>
+        <div
+          className={`${bodyClasses}`}
+          style={{
+            maxWidth: width && `${width}`,
+            width: width && `${width}`,
+          }}
+          ref={ref}
+        >
           <div className="relative">
             {!hideHeader ? (
               <ModalHeader
